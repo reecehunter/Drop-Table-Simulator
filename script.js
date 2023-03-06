2e9cdc4f302a9b102fca01344e60b4c0b1303b51
Reece Hunter <heecerunter@gmail.com>
2023-03-06 10:26:38 -0600

    Complete backend

--- script.js
+++ script.js
@@ -14,46 +14,49 @@ const data = {
   salmon2: 2.875,
   pufferfish1: 8.625,
   pufferfish2: 2.875,
+  clownfish1: 8.625,
+  clownfish2: 2.875,
 };
 
-const simulateDrops = (input, amount, total = 100) => {
+const simulateDrops = (input, amount) => {
   // Define the variable to be returned
   const result = {};
 
   // Define the variable to put the chances into an array to be reduced later
-  const chancesArray = [];
+  const chanceTable = {};
 
   // Copy the keys from our input variable
   for (const key in input) {
     result[key] = 0;
-    chancesArray.push(input[key]);
+    if (!chanceTable[input[key]]) {
+      chanceTable[input[key]] = 1;
+    } else chanceTable[input[key]]++;
   }
 
   // Loop through "amount" times
   for (let i = 0; i < amount; i++) {
-    // Define a random number as our "dice roll" out of the total paramater (100 default)
-    const rand = Math.random() * total;
+    // Set the first rarity to check to the max rarity
+    let rarity = Math.max(...Object.values(input));
+
+    // Define a random number as our "dice roll" out of the max rarity chance
+    const rand = Math.random() * rarity;
 
     // Calculate find the closest rarity in the list of rarity chances
-    // const rarity = Object.values(input).reduce((prev, curr) => {
-    //   console.log(prev, curr);
-    //   return Math.abs(curr - rand) < Math.abs(prev - rand) ? curr : prev;
-    // });
-    let rarity = Object.values(input)[0];
-    for (const key in Object.values(input)) {
-      const value = Object.values(input)[key];
-      // console.log(value, rand / Object.values(input).length);
-      if (value < rand / Object.values(input).length) rarity = value;
+    for (const i in Object.values(input)) {
+      // Define the index we will compare to rand
+      const index = Object.values(input)[i];
+      // Check if index * amount of times it shows up in chance table is greater than rarity
+      if (rand >= index) rarity = index;
     }
 
-    // Get the drop table based on the rarity
+    // Get the drop table based on the calculated rarity
     const droptable = [];
     for (const key in input) {
       if (rarity === input[key]) droptable.push(key);
     }
 
     // Define a random number out of the number of drops in the calculated drop table
-    const rand2 = Math.round(Math.random() * droptable.length);
+    const rand2 = Math.round(Math.random() * (droptable.length - 1));
     // Add the final chosen item to the result
     result[droptable[rand2]]++;
   }
@@ -63,4 +66,4 @@ const simulateDrops = (input, amount, total = 100) => {
 };
 
 // Log the simulated drops result
-console.log(simulateDrops(data, 10000));
+console.log(simulateDrops(data, 100));
